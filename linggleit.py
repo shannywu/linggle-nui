337506c145e0b2d31f562986066a2908c14d8a03
Jane To <janeto@nlplab.cc>
2015-12-17 17:08:20 +0800

    fix 'for' in 'forward' change vnCollocation to query

--- linggleit.py
+++ linggleit.py
@@ -59,6 +59,12 @@ def vpCollocation(headword):
     query = template1 % headword
     return postProcess(start1, query)
 
+def synonym(headword):
+    template1 = '~%s'
+    start1 = 0
+    query = template1 % headword
+    return postProcess(start1, query)
+
 # def allCollocations(headword):
 #     vnCollocation(headword)
 #     anCollocation(headword)
@@ -68,45 +74,52 @@ def vpCollocation(headword):
 Nouns = ['words', 'word', 'nouns', 'noun', 'things', 'thing', 'something', 'example']
 Verbs = ['verbs', 'verb']
 Adjs = ['adjectives', 'ADJ', 'adj']
-Preps = ['preposition', 'prepositions', 'prep', 'prep.']
-wordBeforeTarget = ['describe', 'associate with', 'associates with', 'for', 'go with', 'use with', 'go for', 'use for', 'describe for', 'do for', 'do with', 'describes', 'goes with', 'uses with', 'goes for', 'uses for', 'describes for']
-deleteWord = ['a', 'an', 'the', 'how', 'what', 'is', 'are', 'which', 'I', 'you', 'good', 'best', 'can', 'could', 'should', 'would', 'be', 'What', 'what', 'How', 'how', 'Which', 'which', 'to']
-Synonym = ['same', 'synonyms', 'synonym', 'alike', 'another']
+Preps = ['preposition', 'prepositions', 'prep']
+wordBeforeTarget = ['describe', 'associate with', 'associates with', 'for', 'of', 'go with', 'use with', 'go for', 'use for', 'describe for', 'do for', 'do with', 'describes', 'goes with', 'uses with', 'goes for', 'uses for', 'describes for']
+deleteWord = ['a', 'an', 'the', 'how', 'what', 'is', 'are', 'which', 'I', 'you', 'good', 'best', 'can', 'could', 'should', 'would', 'be', 'What', 'what', 'How', 'how', 'Which', 'which']
+Synonym = ['same', 'syn', 'synonyms', 'synonym', 'alike', 'another']
 
 def transQuery(question):
     que = [ token for token in re.findall("\w+", question)]
     [que.remove(i) for i in deleteWord if i in que]
-    #print que
+    # print que
     speech = ['N' for i in Nouns if i in que]
     speech += ['V' for i in Verbs if i in que]
     speech += ['A' for i in Adjs if i in que]
     speech += ['P' for i in Preps if i in que]
-    speech += ['W' for i in wordBeforeTarget if i in ' '.join(que)]
-    #print speech
-    headword = [' '.join(que[(que.index(i.strip().split(' ')[-1])+1):]) for i in wordBeforeTarget if i in ' '.join(que)]
-    #print headword
+    speech += ['S' for i in Synonym if i in que]
+    speech += ['W' for i in wordBeforeTarget if ' '+i+' ' in ' '.join(que)]
+    # print speech
+    speech_n = list(set(speech))
+    # print speech_n
+    headword = [' '.join(que[(que.index(i.strip().split(' ')[-1])+1):]) for i in wordBeforeTarget if ' '+i+' ' in ' '.join(que)]
+    if '' in headword: headword.remove('')
+    # print headword
 
     finalRes = []
 
-    if 'V' in speech:
-        finalRes.append(['vnCollocation', 'vanCollocation'])
+    if 'V' in speech_n:
+        finalRes.append(['pron. v. ?prep. ?det. '+headword[0], 'v. ?prep. ?det. adj. '+headword[0]])
         finalRes.append(vnCollocation(headword[0]))
         finalRes.append(vanCollocation(headword[0]))
-    elif 'A' in speech:
-        finalRes.append(['anCollocation', 'vanCollocation'])
+    elif 'S' in speech_n:
+        finalRes.append(['~'+headword[0]])
+        finalRes.append(synonym(headword[0]))
+    elif 'P' in speech_n:
+        finalRes.append(['pron. '+headword[0]+' ?prep. ?n.'])
+        finalRes.append(vpCollocation(headword[0]))
+    elif 'A' in speech_n:
+        finalRes.append(['det./prep. adj. '+headword[0], 'v. ?prep. ?det. adj. '+headword[0]])
         finalRes.append(anCollocation(headword[0]))
         finalRes.append(vanCollocation(headword[0]))
-    elif 'N' in speech:
-        finalRes.append(['vnCollocation', 'vanCollocation', 'anCollocation', 'vpCollocation'])
+    elif 'N' in speech_n:
+        finalRes.append(['pron. '+headword[0]+' ?prep. ?n.', 'pron. v. ?prep. ?det. '+headword[0], 'v. ?prep. ?det. adj. '+headword[0], 'det./prep. adj. '+headword[0]])
+        finalRes.append(vpCollocation(headword[0]))
         finalRes.append(vnCollocation(headword[0]))
         finalRes.append(vanCollocation(headword[0]))
         finalRes.append(anCollocation(headword[0]))
-        finalRes.append(vpCollocation(headword[0]))
-    elif 'P' in speech:
-        finalRes.append(['vpCollocation'])
-        finalRes.append(vpCollocation(headword[0]))
-    elif 'W' in speech:
-        finalRes.append(['anCollocation', 'vanCollocation'])
+    elif 'W' in speech_n:
+        finalRes.append(['det./prep. adj. '+headword[0], 'v. ?prep. ?det. adj. '+headword[0]])
         finalRes.append(anCollocation(headword[0]))
         finalRes.append(vanCollocation(headword[0]))
     else:
@@ -114,12 +127,14 @@ def transQuery(question):
     
     return finalRes
 
-# if __name__ == '__main__':
-#     while True:
-#         query = raw_input(">>(type 'EX' to exit)\n>>query: ")
-#         if query == 'EX': break
-#         else: 
-#             for q in transQuery(query):
-#                 for i in q:
-#                     print '{}\t{}'.format(i[0],i[1])
-#                 print '========================================'
+if __name__ == '__main__':
+    while True:
+        query = raw_input(">>(type 'EX' to exit)\n>>query: ")
+        if query == 'EX': break
+        else: 
+            print transQuery(query)[0]
+            print '------------------------------------------------'
+            for q in transQuery(query)[1:]:
+                for i in q:
+                    print '{}\t{}'.format(i[0],i[1])
+                print '========================================'
